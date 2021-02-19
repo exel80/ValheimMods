3f921ee9e98c77604567e49cf9cfb62eea96cbda
exel80 <theexel80@gmail.com>
2021-02-19 17:51:28 +0200

    New mod "MasterMode" (WIP)

---
+++ MasterMode/MasterMode.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+using static ItemDrop.ItemData;
+
+/**
+ * Original idea from Aurarus#3253
+ * "I'm wondering how difficult it'd be to make a "Master mode" mod for valheim"
+ * 
+ * X = Done
+ * % = Half way done OR working on it
+ *   = (empty) Not started yet
+ * 
+ * TODO:
+ * [] All enemies besides deer and boar spawn as one/ two star or higher
+ * [X] Crafting recipes for gear are doubled in price/ [?] include harder to find materials
+ * [X] Default health without food is 1 instead of 25
+ * [] Bosses deal more damage and have faster attack animations
+*/
+
+namespace MasterMode
+{
+    [BepInPlugin("dev.exel80.mastermode", "Master Mode", "1.0.0")]
+    public class MasterModePluign : BaseUnityPlugin
+    {
+        private static ConfigFile config;
+
+        private static ManualLogSource logSource;
+
+        private static Dictionary<ItemType, Int32> _multiplier;
+
+        private void Awake()
+        {
+            config = Config;
+
+            logSource = Logger;
+
+            _multiplier = InitializeResourceMultipliers();
+
+            //_multiplier.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(UnityEngine.Debug.Log);
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        private Dictionary<ItemType, Int32> InitializeResourceMultipliers()
+        {
+            // Create blank Dictionary
+            Dictionary<ItemType, Int32> _multipliers = new Dictionary<ItemType, Int32>();
+
+            // Gear list of ItemTypes. Used to modify default value of multiplier from 2 to 3
+            List<ItemType> _gear_list = new List<ItemType>() { ItemType.Helmet, ItemType.Chest, ItemType.Shoulder, ItemType.Legs };
+
+            // Fill Dictionary with all of the ItemTypes
+            foreach (ItemType type in Enum.GetValues(typeof(ItemType)))
+            {
+                // Name of the type
+                string typeName = type.ToString();
+
+                // Resource multiplier default value
+                int multiplier = _gear_list.Contains(type) ? 3 : 2;
+
+                // Fetch multiplier value from config file
+                ConfigEntry<Int32> typeData = Config.Bind(
+                    "RecipeMultiplier", 
+                    typeName,
+                    multiplier,
+                    $"{typeName} recipe resource multiplier"
+                );
+
+                // Add multiplier value to the Dictionary
+                _multipliers.Add(type, typeData.Value);
+            }
+
+            // Return Dictionary
+            return _multipliers;
+        }
+
+        public static Dictionary<ItemType, Int32> GetMultipliers() => _multiplier;
+
+        public static void Log(LogLevel level, object data) => logSource.Log(level, data);
+
+        public static ConfigFile GetConfig() => config;
+    }
+}
---
+++ MasterMode/PlayerStats.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BepInEx.Configuration;
+using HarmonyLib;
+using static Player;
+
+namespace MasterMode
+{
+    [HarmonyPatch(typeof(Player), "GetTotalFoodValue")]
+    static class PlayerStats_GetTotalFoodValue_Patch
+    {
+        [HarmonyPostfix]
+        static void Postfix(Player __instance, ref float hp)
+        {
+            /**
+             *  Modify how much base health a player has. Just make sure to recalculate food
+             *  Thank you Xenofell#7360 for showing that Player:GetTotalFoodValue exist
+             */
+
+            ConfigEntry<Int32> baseHealth = MasterModePluign.GetConfig().Bind("Player", "BaseHealth", 1, "Player base health when haven't eat anything");
+
+            // Base Health
+            hp = (float)baseHealth.Value;
+
+            // Recalculate food
+            foreach (Food food in __instance.GetFoods())
+            {
+                hp += food.m_health;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), "GetBaseFoodHP")]
+    static class PlayerStats_GetBaseFoodHP_Patch
+    {
+        [HarmonyPostfix]
+        static void Postfix(ref float __result)
+        {
+            /*
+             *  This modify "white health bar" next to the current health.
+             *  Modifying this value doesn't affect how much heal you regenerate, it is just a visual.
+             */
+
+            ConfigEntry<Int32> baseHealth = MasterModePluign.GetConfig().Bind("Player", "BaseHealth", 1, "Player base health when haven't eat anything");
+
+            __result = (float)baseHealth.Value;
+        }
+    }
+}
