8e389c63aef6dced60fdaf32155556a3f5895d53
exel80 <theexel80@gmail.com>
2021-02-25 13:31:59 +0200

    Updated MapTeleported plugin. - Added hidePing config - Added README & manifest.json

--- Teleporter/Teleporter.cs
+++ Teleporter/Teleporter.cs
@@ -4,27 +4,34 @@ using HarmonyLib;
 using System.Reflection;
 using UnityEngine;
 using System.Collections;
+using BepInEx.Configuration;
 
 namespace Teleporter
 {
     [BepInPlugin("dev.exel80.mapteleporter", "MapTeleporter", "1.0.0")]
     public class Teleporter : BaseUnityPlugin
     {
+        public static ConfigEntry<bool> hidePing;
+
         void Awake()
         {
+            hidePing = Config.Bind("Ping", "hidePing", false, "Hide ping when using MapTeleporter plugin");
+
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
         }
 
         [HarmonyPatch(typeof(Chat), nameof(Chat.SendPing))]
         static class Chat_SendPing_Patch
         {
-            static void Prefix(Chat __instance, ref Vector3 position)
+            static bool Prefix(Chat __instance, ref Vector3 position)
             {
                 if (Input.GetKey(KeyCode.LeftControl))
                 {
                     Player localPlayer = Player.m_localPlayer;
                     localPlayer.TeleportTo(position, localPlayer.transform.rotation, true);
+                    return !hidePing.Value;
                 }
+                return true;
             }
         }
     }
